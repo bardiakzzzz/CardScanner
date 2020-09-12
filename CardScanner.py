d85d89d94b88fa2d363384683272ab42dffc4cd4
bardia karimi <bardiakarimi7@gmail.com>
2020-09-12 11:31:03 +0430

    Update CardScanner

--- CardScanner.py
+++ CardScanner.py
@@ -10,13 +10,13 @@ ap.add_argument("-i", "--image", required=True,
                 help="Path to the image to be scanned")
 args = vars(ap.parse_args())
 
+#read image and resize it
 image = cv2.imread(args["image"])
-
-# image = cv2.imread('FirstPart/photo_2019-01-26_11-49-40.jpg')
 ratio = image.shape[0] / 500.0
 orig = image.copy()
 image = imutils.resize(image, height=500)
 
+#insert GaussianBlue to make image more smooth
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
@@ -24,12 +24,14 @@ cv2.imshow("blur", gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#insert Canny edge detection in order to find Card
 edged = cv2.Canny(gray, 50, 250)
 
 cv2.imshow("Canny", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#insert hough line transform in order to fill incomplete lines
 lines = cv2.HoughLinesP(image=edged, rho=1, theta=np.pi / 180, threshold=100,
                         minLineLength=50, maxLineGap=50)
 
@@ -43,9 +45,11 @@ cv2.imshow("Edged", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#use findContours to find all rectangles
 contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 
+#sort rectangles in order to find the biggest one
 contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
 for c in contours:
@@ -55,13 +59,14 @@ for c in contours:
         screenCnt = approx
         break
 
+#if the card is the biggest rectangle you will see the results
 cv2.drawContours(image, [screenCnt], -1, (255, 255, 0), 5)
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#use four point transform to get the card
 new_contours = screenCnt.reshape(4, 2) * ratio
-
 warped = four_point_transform(orig, new_contours)
 
 cv2.imshow("Scanned", warped)
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # CardScanner
+Find card in a picture using computer vision operations like Hough line transform and Perspective transform
+
+![Alt Text](https://github.com/bardiakzzzz/CardScanner/blob/master/images/1.jpg)
+![Alt Text](https://github.com/bardiakzzzz/CardScanner/blob/master/images/2.png)
+![Alt Text](https://github.com/bardiakzzzz/CardScanner/blob/master/images/3.png)
+![Alt Text](https://github.com/bardiakzzzz/CardScanner/blob/master/images/4.png)
+![Alt Text](https://github.com/bardiakzzzz/CardScanner/blob/master/images/5.png)
 
 ## Requirements
 
